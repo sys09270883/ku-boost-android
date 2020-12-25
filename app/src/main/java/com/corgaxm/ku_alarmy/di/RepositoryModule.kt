66ab84099f0b6b62d89d5c6722c1610d5c05f9f7
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-12-26 08:36:39 +0900

    코드 리팩토링: Repository class -> interface

--- app/src/main/java/com/corgaxm/ku_alarmy/di/RepositoryModule.kt
+++ app/src/main/java/com/corgaxm/ku_alarmy/di/RepositoryModule.kt
@@ -1,8 +1,30 @@
 package com.corgaxm.ku_alarmy.di
 
+import com.corgaxm.ku_alarmy.api.LoginService
 import com.corgaxm.ku_alarmy.data.login.LoginRepository
+import com.corgaxm.ku_alarmy.data.login.LoginResponse
+import com.corgaxm.ku_alarmy.utils.Resource
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single { LoginRepository(get()) }
+    fun provideLoginRepository(loginService: LoginService): LoginRepository {
+        return object : LoginRepository {
+            override suspend fun makeLoginRequest(
+                username: String,
+                password: String
+            ): Resource<LoginResponse> {
+                val loginResponse = loginService.login(username, password)
+                val loginSuccess = loginResponse.loginSuccess
+                val loginFailure = loginResponse.loginFailure
+
+                return when {
+                    loginSuccess?.isSucceeded == true -> Resource.success(loginResponse)
+                    loginFailure != null -> Resource.error(loginFailure.errorMessage)
+                    else -> Resource.error("서버 에러입니다.\n잠시 후 시도해주세요.")
+                }
+            }
+        }
+    }
+
+    single { provideLoginRepository(get()) }
 }
