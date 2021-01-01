4f45ad3dc0f4f20ecfffb4578a791aadbad40b60
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-01-01 20:53:25 +0900

    데이터베이스 스키마 export 제한

--- app/src/main/java/com/konkuk/boost/persistence/AppDatabase.kt
+++ app/src/main/java/com/konkuk/boost/persistence/AppDatabase.kt
@@ -3,7 +3,11 @@ package com.konkuk.boost.persistence
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [GraduationSimulationEntity::class, GradeEntity::class], version = 1)
+@Database(
+    entities = [GraduationSimulationEntity::class, GradeEntity::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun graduationSimulationDao(): GraduationSimulationDao
     abstract fun gradeDao(): GradeDao
