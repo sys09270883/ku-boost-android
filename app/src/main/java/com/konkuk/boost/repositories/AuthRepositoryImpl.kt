8cc8cb20985fed54b2e76332ad58b677408c0b3f
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-01-13 22:13:03 +0900

    자동로그인 중복 코드 제거

--- app/src/main/java/com/konkuk/boost/repositories/AuthRepositoryImpl.kt
+++ app/src/main/java/com/konkuk/boost/repositories/AuthRepositoryImpl.kt
@@ -45,34 +45,7 @@ class AuthRepositoryImpl(
     override suspend fun makeAutoLoginRequest(): UseCase<LoginResponse> {
         val username = preferenceManager.getUsername()
         val password = preferenceManager.getPassword()
-
-        if (username.isBlank() || password.isBlank()) {
-            preferenceManager.setAuthInfo("", "")
-            return UseCase.error("아이디, 비밀번호를 확인하세요.")
-        }
-
-        val loginResponse: Response<LoginResponse>
-        try {
-            loginResponse = authService.login(username, password)
-        } catch (e: Exception) {
-            Log.e("ku-boost", "${e.message}")
-            return UseCase.error("네트워크를 확인하세요.")
-        }
-
-        val cookie = loginResponse.headers()["Set-Cookie"]?.split(";")?.first()
-            ?: return UseCase.error("다시 로그인하세요.")
-        preferenceManager.setCookie(cookie)
-
-        val loginBody = loginResponse.body()
-        val loginSuccess =
-            loginBody?.loginSuccess ?: return UseCase.error("아이디, 비밀번호를 확인하세요.")
-        val loginFailure = loginBody.loginFailure
-
-        return when {
-            loginSuccess.isSucceeded -> UseCase.success(loginBody)
-            loginFailure != null -> UseCase.error(loginFailure.errorMessage)
-            else -> UseCase.error("서버에 문제가 발생했습니다.")
-        }
+        return makeLoginRequest(username, password)
     }
 
     override suspend fun makeLogoutRequest(): UseCase<Unit> {
