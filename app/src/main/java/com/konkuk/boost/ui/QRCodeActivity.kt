038da876cf1fdb9367b0d360f18b5428ff3470fc
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-01-18 19:11:09 +0900

    QR 코드 생성

--- app/src/main/java/com/konkuk/boost/ui/QRCodeActivity.kt
+++ app/src/main/java/com/konkuk/boost/ui/QRCodeActivity.kt
@@ -1,10 +1,15 @@
 package com.konkuk.boost.ui
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.snackbar.Snackbar
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
 import com.konkuk.boost.R
 import com.konkuk.boost.databinding.ActivityQRCodeBinding
 import com.konkuk.boost.utils.UseCase
@@ -31,15 +36,34 @@ class QRCodeActivity : AppCompatActivity() {
         viewModel.qrResponse.observe(this) {
             when (it.status) {
                 UseCase.Status.SUCCESS -> {
-                    Log.d("yoonseop", "data: ${it.data?.data}")
-                    Log.d("yoonseop", "membership: ${it.data?.data?.membershipCard}")
-                    Log.d("yoonseop", "thumbnail: ${it.data?.data?.thumbnailUrl}")
+                    val membershipCard = it.data?.data?.membershipCard
+                    val thumbnailUrl = it.data?.data?.thumbnailUrl
+                    val bitmap = generateQRCode("M-${membershipCard}")
+                    binding.qrImageView.setImageBitmap(bitmap)
                 }
                 UseCase.Status.ERROR -> {
                     Snackbar.make(binding.container, "${it.message}", Snackbar.LENGTH_SHORT).show()
                 }
             }
         }
+    }
+
+    private fun generateQRCode(text: String): Bitmap {
+        val width = 500
+        val height = 500
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val codeWriter = MultiFormatWriter()
+        try {
+            val bitMatrix = codeWriter.encode(text, BarcodeFormat.QR_CODE, width, height)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+                }
+            }
+        } catch (e: WriterException) {
+            Log.d("yoonseop", "generateQRCode: ${e.message}")
+        }
+        return bitmap
     }
 
 }
