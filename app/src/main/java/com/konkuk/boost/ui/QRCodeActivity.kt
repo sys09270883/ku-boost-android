2ab30478fc8cba93834f619ba3ae9c11959fc1af
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-01-18 19:39:34 +0900

    모바일 열람증 썸네일 추가

--- app/src/main/java/com/konkuk/boost/ui/QRCodeActivity.kt
+++ app/src/main/java/com/konkuk/boost/ui/QRCodeActivity.kt
@@ -6,15 +6,19 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 import com.konkuk.boost.R
 import com.konkuk.boost.databinding.ActivityQRCodeBinding
 import com.konkuk.boost.utils.UseCase
 import com.konkuk.boost.viewmodels.QRCodeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
 
 class QRCodeActivity : AppCompatActivity() {
 
@@ -36,9 +40,10 @@ class QRCodeActivity : AppCompatActivity() {
         viewModel.qrResponse.observe(this) {
             when (it.status) {
                 UseCase.Status.SUCCESS -> {
-                    val membershipCard = it.data?.data?.membershipCard
-                    val thumbnailUrl = it.data?.data?.thumbnailUrl
-                    val bitmap = generateQRCode("M-${membershipCard}")
+                    val membershipCard = it.data?.data?.membershipCard ?: return@observe
+                    val thumbnailUrl = it.data.data.thumbnailUrl
+                    val bitmap = generateQRCode(membershipCard)
+                    Glide.with(this).load(thumbnailUrl).into(binding.thumbnailImageView)
                     binding.qrImageView.setImageBitmap(bitmap)
                 }
                 UseCase.Status.ERROR -> {
@@ -53,15 +58,17 @@ class QRCodeActivity : AppCompatActivity() {
         val height = 500
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val codeWriter = MultiFormatWriter()
+        val hints = Hashtable<EncodeHintType, ErrorCorrectionLevel>()
+        hints[EncodeHintType.ERROR_CORRECTION] = ErrorCorrectionLevel.M
         try {
-            val bitMatrix = codeWriter.encode(text, BarcodeFormat.QR_CODE, width, height)
+            val bitMatrix = codeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints)
             for (x in 0 until width) {
                 for (y in 0 until height) {
                     bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
                 }
             }
         } catch (e: WriterException) {
-            Log.d("yoonseop", "generateQRCode: ${e.message}")
+            Log.d("ku-boost", "generateQRCode: ${e.message}")
         }
         return bitmap
     }
