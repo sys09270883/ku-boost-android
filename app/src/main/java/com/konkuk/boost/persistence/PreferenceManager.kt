68ce2e576e5104dd1c2d5ac0f50e3547c327454d
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-01-21 11:49:48 +0900

    버그 픽스: 초기 학기 값 1로 설정

--- app/src/main/java/com/konkuk/boost/persistence/PreferenceManager.kt
+++ app/src/main/java/com/konkuk/boost/persistence/PreferenceManager.kt
@@ -29,6 +29,7 @@ class PreferenceManager(private val context: Context) {
         private const val DEFAULT_STRING = ""
         private const val DEFAULT_INT = 2021
         private const val DEFAULT_BOOLEAN = false
+        private const val DEFAULT_SEMESTER = 1
     }
 
     private val pref: SharedPreferences by lazy { getEncryptedSharedPreference() }
@@ -42,7 +43,7 @@ class PreferenceManager(private val context: Context) {
     var code: String by pref.stringPreference(CODE)
     var hasData: Boolean by pref.booleanPreference(HAS_DATA)
     var accessToken: String by pref.stringPreference(ACCESS_TOKEN)
-    var selectedSemester: Int by pref.intPreference(SELECTED_SEMESTER)
+    var selectedSemester: Int by pref.intPreference(SELECTED_SEMESTER, DEFAULT_SEMESTER)
 
     private fun getEncryptedSharedPreference(): SharedPreferences {
         val keyGenParameterSpec = KeyGenParameterSpec.Builder(
@@ -77,6 +78,7 @@ class PreferenceManager(private val context: Context) {
         this.state = state
         this.dept = dept
         this.code = code
+        this.selectedSemester = DEFAULT_SEMESTER
     }
 
     fun clearAll() {
