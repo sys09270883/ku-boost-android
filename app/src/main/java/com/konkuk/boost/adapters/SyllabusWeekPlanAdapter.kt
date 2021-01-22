a9928996bafd791ed862715be2666e895b08170f
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-01-22 09:28:37 +0900

    강의계획서 주별강의계획 글자 짤림 해결

--- app/src/main/java/com/konkuk/boost/adapters/SyllabusWeekPlanAdapter.kt
+++ app/src/main/java/com/konkuk/boost/adapters/SyllabusWeekPlanAdapter.kt
@@ -1,6 +1,7 @@
 package com.konkuk.boost.adapters
 
 import android.annotation.SuppressLint
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -38,9 +39,13 @@ class SyllabusWeekPlanAdapter :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlanViewHolder {
-        return PlanViewHolder(
+        val holder = PlanViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.week_plan_item, parent, false)
         )
+
+        holder.contentTextView.movementMethod = ScrollingMovementMethod()
+
+        return holder
     }
 
     @SuppressLint("SetTextI18n")
