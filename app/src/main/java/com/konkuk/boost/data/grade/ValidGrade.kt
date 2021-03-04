61b417384d8458907be0c72718940d3c119b8a21
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-03-04 16:50:40 +0900

    유효한 과목 PK 오류 수정

--- app/src/main/java/com/konkuk/boost/data/grade/ValidGrade.kt
+++ app/src/main/java/com/konkuk/boost/data/grade/ValidGrade.kt
@@ -3,5 +3,7 @@ package com.konkuk.boost.data.grade
 import com.google.gson.annotations.SerializedName
 
 data class ValidGrade(
-    @SerializedName("SBJT_ID") val subjectId: String
+    @SerializedName("SBJT_ID") val subjectId: String,
+    @SerializedName("YY") val year: String,
+    @SerializedName("SHTM") val semesterCode: String,
 )
