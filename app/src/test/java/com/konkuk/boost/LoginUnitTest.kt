f1a489451a01dd0231ec9077cb18c939d9fd34e0
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-03-05 17:06:30 +0900

    Unit test naming 수정

--- app/src/test/java/com/konkuk/boost/LoginUnitTest.kt
+++ app/src/test/java/com/konkuk/boost/LoginUnitTest.kt
@@ -8,7 +8,7 @@ import org.junit.Test
 class LoginUnitTest {
 
     @Test
-    fun `login_isSuccess`() {
+    fun loginIsSuccess() {
         val loginSuccessResponse = LoginResponse(
             LoginSuccess(
                 true
@@ -24,7 +24,7 @@ class LoginUnitTest {
     }
 
     @Test
-    fun `login_changePasswordAfter90Days`() {
+    fun loginChangePasswordAfter90Days() {
         val loginFailedResponse = LoginResponse(
             null, LoginFailure(
                 "비밀번호 변경 후 90일이 지났습니다. 비밀번호를 변경해주세요.",
@@ -42,7 +42,7 @@ class LoginUnitTest {
     }
 
     @Test
-    fun `login_invalidPassword`() {
+    fun loginInvalidPassword() {
         val loginFailedResponse = LoginResponse(
             null, LoginFailure(
                 "로그인 실패하였습니다.(1회)\n*5회이상 실패시 접속불가*",
